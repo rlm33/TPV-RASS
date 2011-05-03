bf6c9410a3d5d46be36ab5a91e5a42695cd72343
HanHelsing <hanhelsing@gmail.com>
2011-05-03 13:52:21 +0200

    He puesto los atributos como privados y he puesto DiaSemana como un ArrayList, ya que no tiene por qué ser sólo un día, sino que pueden ser varios: L,M,J,V, por ejemplo. Además se ha añadido un constructor sobrecargado y los get/set de cada atributo. Como diaSemana ha pasado a ser un arrayList he insertado: set para cambiar toda la lista y add para añadir tan sólo un atributo.
    Un saludo!

--- src/principal/DiaDescuento.java
+++ src/principal/DiaDescuento.java
@@ -1,16 +1,81 @@
 package principal;
 
+import java.util.ArrayList;
+
+/**
+ * Esta clase nos marca los días en los cuáles se les puede aplicar descuento a una determinada persona.
+ * @author RASS
+ *
+ */
 public class DiaDescuento {
 
-	DiaSemana dia;
-	int horaInicio;
-	int horaFin;
+	//Pueden ser varios días, por eso un ArrayList
+	private ArrayList<DiaSemana> dia;
+	private int horaInicio;
+	private int horaFin;
 	
-	public DiaDescuento(DiaSemana dia, int horaInicio, int horaFin) {
-		
-		this.dia = dia;
+	/*
+	 * CONSTRUCTORES
+	 */
+	public DiaDescuento(ArrayList<DiaSemana> dia, int horaInicio, int horaFin) {
+		if(this.dia.isEmpty()){
+			this.dia = new ArrayList<DiaSemana>();
+		} else {
+			this.dia.clear();
+		}
+		this.dia.addAll(dia);
 		this.horaInicio = horaInicio;
 		this.horaFin = horaFin;
+	}
+	
+	public DiaDescuento(DiaDescuento diaDescuento) {
+		if(this.dia.isEmpty()){
+			this.dia = new ArrayList<DiaSemana>();
+		} else {
+			this.dia.clear();
+		}
+		this.dia.addAll(diaDescuento.getDias());
+		this.horaInicio = diaDescuento.getHoraInicio();
+		this.horaFin = diaDescuento.getHoraFin();
+	}
+
+	/*
+	 * GETTERS AND SETTERS
+	 */
+	
+	//Añade un único día
+	public void addDia(DiaSemana dia) {
+		this.dia.add(dia);
+	}
+	
+	//Añade todos los días a la vez
+	public void setDias(ArrayList<DiaSemana> dias){
+		if(this.dia.isEmpty()){
+			this.dia = new ArrayList<DiaSemana>();
+		} else {
+			this.dia.clear();
+		}
+		this.dia.addAll(dias);
+	}
+
+	public ArrayList<DiaSemana> getDias() {
+		return dia;
+	}
+
+	public void setHoraInicio(int horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public int getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraFin(int horaFin) {
+		this.horaFin = horaFin;
+	}
+
+	public int getHoraFin() {
+		return horaFin;
 	}
 	
 	
