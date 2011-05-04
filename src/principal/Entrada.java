e004130e31a3b41c703e5967186391efcebf53d0
HanHelsing <hanhelsing@gmail.com>
2011-05-04 13:45:56 +0200

    Un par de cambios, creo que ya está bien

--- src/principal/Entrada.java
+++ src/principal/Entrada.java
@@ -5,40 +5,41 @@ import java.util.ArrayList;
 /**
  * Se trata de la clase abstracta del constructor Builder de las Entradas. De momento las entradas serán de tipo XML, pero si en un futuro
  * se quisieran hacer de otro tipo HTML, hojas de texto, etc se podría extender el programa añadiendo tan sólo otra clase al sistema de clases.
- * @author HanHelsing
+ * @author RASS
  *
  */
 abstract class Entrada {
 	protected Pedido pedido;
 	
 	public void crearNuevoPedido(){
-		pedido = new Pedido();
-		buildDiaDescuento();
-		buildSetLinVentas();
-		buildTarjeta();
-		buildEmpleado();		
+		pedido = new Pedido();		
 	}
 	
 	public Pedido getPedido(){
 		return pedido;
 	}
 	
+	public DiaDescuento getDiaDescuento(){
+		return pedido.getDiaDescuento();
+	}
+	
+	public ArrayList<LinVenta> getLinVentas(){
+		return pedido.getLinVentas();
+	}
+	
+	public Tarjeta getTarjeta(){
+		return pedido.getTarjeta();
+	}
+	
+	public boolean getEmpleado(){
+		return pedido.getEmpleado();
+	}
+	
 	public abstract void buildDiaDescuento();
-	
-	public abstract DiaDescuento getDiaDescuento();
-	
-	public abstract void buildSetLinVentas();
 	
 	public abstract void buildAddLinVentas();
 	
-	public abstract ArrayList<LinVenta> getLinVentas();
-	
 	public abstract void buildTarjeta();
 	
-	public abstract Tarjeta getTarjeta();
-	
 	public abstract void buildEmpleado();
-	
-	public abstract boolean getEmpleado();
-
 }
