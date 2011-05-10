5b8045439b4538b49ea85b77629192010ad8a5ca
rlm33 <maeseraitokun@gmail.com>
2011-05-10 19:37:37 +0200

    Change-Id: Icd012989058f6a58436cf4d614ca74a54f23fd37

---
+++ src/principal/CajaRegistradora.java
@@ -0,0 +1,17 @@
+package principal;
+
+public class CajaRegistradora {
+	
+	private int id; //Por ponerle algo
+	
+	public CajaRegistradora(int id)
+	{
+		this.id = id;
+	}
+	
+	public int getId()
+	{
+		return this.id;
+	}
+
+}
