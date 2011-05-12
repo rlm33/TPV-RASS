58eba20f34b6b8759ece611cc3902290c66c53cd
HanHelsing <hanhelsing@gmail.com>
2011-05-12 11:25:16 +0200

    Salida Fichero

---
+++ src/principal/Salida.java
@@ -0,0 +1,16 @@
+package principal;
+
+import java.util.ArrayList;
+
+/**
+ * Se trata de la clase abstracta del constructor de las Entradas.
+ * @author RASS
+ *
+ */
+abstract class Salida {
+	public Salida(){
+		
+	}
+	
+	public abstract void setVenta(Venta v);
+}
