7d3bf29eeaca106129ee1f0ac63e9e1912a4647c
rlm33 <maeseraitokun@gmail.com>
2011-05-13 20:16:16 +0200

    Añadido pruebaEntrada.
    Change-Id: I7a7ddde1c9eb5b83aca6570cc07a5cc5936ebe51

---
+++ src/pruebas/pruebaEntrada.java
@@ -0,0 +1,34 @@
+package pruebas;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import principal.Sistema;
+
+public class pruebaEntrada {
+	
+	public static void Main(String args[])
+	{
+		Sistema s = Sistema.getInstancia();
+		
+		try {
+			
+			s.inicializar("CAJA1", "ficheroVenta.xml", "pruebaEntrada.xml");
+			s.run();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
