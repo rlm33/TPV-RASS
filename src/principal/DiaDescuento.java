b410a34bd7155901257f7a14f0605213aaca6f8a
rlm33 <maeseraitokun@gmail.com>
2011-05-14 18:25:15 +0200

    Change-Id: Ifff0bafb8027da483aa6049481bf55d8266e97ed

---
+++ src/principal/DiaDescuento.java
@@ -0,0 +1,90 @@
+package principal;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import utilidades.Propiedades;
+
+
+
+/**
+ * Esta clase implementa un descuento segun la ubicacion temporal que contiene un dia de la semana y un horario.
+ * @author RASS
+ *
+ * Cada Descuento de un dia diferente contarlo como otro objeto de tipo DiaDescuento.
+ * De este modo si queremos que haya descuento los lunes de 10 a 12 y los martes de 9 a 11 simplemente
+ * meteremos dos DiaDescuento en el array.
+ */
+public class DiaDescuento extends Descuento {
+
+
+	private DiaSemana dia;
+	private int horaInicio;
+	private int horaFin;
+
+	/*
+	 * CONSTRUCTORES
+	 */
+	public DiaDescuento(){
+		this.dia = DiaSemana.NULO;
+		this.horaInicio = -1;
+		this.horaFin = -1;
+	}
+
+	public DiaDescuento(DiaSemana dia, int horaInicio, int horaFin) {
+		this.dia = dia;
+		this.horaInicio = horaInicio;
+		this.horaFin = horaFin;
+	}
+
+	public DiaDescuento(DiaDescuento diaDescuento) {
+		this.dia =  diaDescuento.dia;
+		this.horaInicio = diaDescuento.getHoraInicio();
+		this.horaFin = diaDescuento.getHoraFin();
+	}
+
+	/*
+	 * GETTERS AND SETTERS
+	 */
+
+	public void setDia(DiaSemana dia)
+	{
+		this.dia = dia;
+	}
+
+	public DiaSemana getDia() {
+		return dia;
+	}
+
+	public void setHoraInicio(int horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public int getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraFin(int horaFin) {
+		this.horaFin = horaFin;
+	}
+
+	public int getHoraFin() {
+		return horaFin;
+	}
+	
+	@Override
+	public float aplicarDescuento(float desc)
+	{
+		//Properties p = Propiedades.getInstancia().getPropiedades();
+		try {
+			Propiedades.getProperty("dtoMiercoles");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// TODO Mirar si estamos en dia y hora de descuento
+		//if(this.dia.equals())
+		return desc;
+	}
+
+}
