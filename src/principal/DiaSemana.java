93df4897a57fa0b4c8bb6da4203cb13690104520
HanHelsing <hanhelsing@gmail.com>
2011-05-14 17:53:15 +0200

    Comentarios

--- src/principal/DiaSemana.java
+++ src/principal/DiaSemana.java
@@ -1,6 +1,11 @@
 package principal;
 
+/**
+ * Enum de los distintos días de la semana
+ * @author RASS
+ *
+ */
 public enum DiaSemana {
 
-	LUNES,MARTES,MIERCOLES,JUEVES,VIERNES,SABADO,DOMINGO,NULO
+	LUNES,MARTES,MIERCOLES,JUEVES,VIERNES,SABADO,DOMINGO,NULO;
 }
