d90e06bab33291f047085ca497b762180edc8574
HanHelsing <hanhelsing@gmail.com>
2011-05-15 21:02:41 +0200

    Quitadas librerías innecesarias

--- src/principal/EntradaXML.java
+++ src/principal/EntradaXML.java
@@ -1,17 +1,13 @@
 package principal;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.xerces.parsers.DOMParser;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
